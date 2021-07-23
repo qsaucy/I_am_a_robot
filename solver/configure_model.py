4fbd1f6d5f3ee2f51b705e8befd2b644995d9355
quentin.saucy <quentin.saucy@heig-vd.ch>
2021-07-23 15:57:21 +0200

    renaming model

--- solver/configure_model.py
+++ solver/configure_model.py
@@ -2,8 +2,8 @@ from config import DELIMITER
 DEFAULT_MODEL = DELIMITER+"solver"+DELIMITER+"model"+DELIMITER+"cityscape_work_well.pt"
 DEFAULT_MODEL_KNOWNLEDGE = ['bicycle','car','truck', 'bus', 'motorcycle', 'traffic light','vehicles']
 SPECIFIC_MODEL = {
-    "bicycle":DELIMITER+"solver"+DELIMITER+"model"+DELIMITER+"bicycle_shufflenet_v2_x0_5.pt",
-    "bus":DELIMITER+"solver"+DELIMITER+"model"+DELIMITER+"bus_shufflenet_v2_x0_5.pt",
+    "bicycle":DELIMITER+"solver"+DELIMITER+"model"+DELIMITER+"bicycle_vgg13.pt",
+    "bus":DELIMITER+"solver"+DELIMITER+"model"+DELIMITER+"bus_vgg13.pt",
     "car":DELIMITER+"solver"+DELIMITER+"model"+DELIMITER+"car_shufflenet_v2_x0_5.pt",
     "crosswalk":DELIMITER+"solver"+DELIMITER+"model"+DELIMITER+"crosswalk_shufflenet_v2_x0_5.pt",
     "a fire hydrant":DELIMITER+"solver"+DELIMITER+"model"+DELIMITER+"fire_hydrant_shufflenet_v2_x0_5.pt"
